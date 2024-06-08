0375d0025db47630c406fea6ea390d32ad0fa104
Łukasz Dąbek <ukidabek@gmail.com>
2024-06-08 15:27:56 +0200

    [Code] Context injection logic updated.
    * Context injection logic moved to dedicated class.
    * Bindings flags updated to handle injection of inherited fields.

---
+++ Runtime/Core/ContextHandler.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Utilities.States
+{
+    public class ContextHandler
+	{
+		private const BindingFlags Binding_Flags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.FlattenHierarchy | BindingFlags.Instance;
+
+		private bool ValidateType(Context context, MemberInfo member)
+		{
+			var contextType = context.Type;
+			Type memberType = default;
+
+			switch (member)
+			{
+				case FieldInfo fieldInfo:
+					memberType = fieldInfo.FieldType;
+					break;
+				case PropertyInfo propertyInfo:
+					memberType = propertyInfo.PropertyType;
+					break;
+			}
+
+			if (memberType.IsInterface)
+			{
+				var interfaces = contextType.GetInterfaces();
+				return interfaces.Contains(memberType);
+			}
+
+			return contextType == memberType;
+		}
+
+		public void FillState(IState state, IEnumerable<Context> contexts)
+		{
+			var contextDestinations = state.GetContextDestination();
+			foreach (var logic in contextDestinations)
+			{
+				var logicType = logic.GetType();
+				var members = logicType.GetMembers(Binding_Flags);
+
+				foreach (var member in members)
+				{
+					var attribute = member.GetCustomAttribute<ContextField>();
+					if (attribute == null) continue;
+
+					Context context = default;
+					var attributeID = attribute.ID;
+					if (string.IsNullOrEmpty(attributeID))
+					{
+						context = contexts.FirstOrDefault(context => ValidateType(context, member));
+						if (context == null)
+							continue;
+					}
+					else
+					{
+						context = contexts.FirstOrDefault(context => ValidateType(context, member) && context.Id == attributeID);
+						if (context == null)
+							continue;
+					}
+
+					switch (member)
+					{
+						case FieldInfo fieldInfo:
+							fieldInfo.SetValue(logic, context.Object);
+							break;
+						case PropertyInfo propertyInfo:
+							propertyInfo.SetValue(logic, context.Object);
+							break;
+					}
+				}
+			}
+		}
+	}
+}
--- Runtime/Core/StateMachine.cs
+++ Runtime/Core/StateMachine.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace Utilities.States
 {
-	public class StateMachine : IStateMachine, IDisposable
-    {
-		private const BindingFlags Binding_Flags = BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance;
-
-        public event Action<IState> OnStateChanged;
+    public class StateMachine : IStateMachine, IDisposable
+	{
+		private ContextHandler m_contextHandler = new ContextHandler();
+		public event Action<IState> OnStateChanged;
 
 		private readonly IEnumerable<IStateLogicExecutor> m_stateLogicExecutor = null;
-        private readonly IEnumerable<IStateTransition> m_transitions = null;
+		private readonly IEnumerable<IStateTransition> m_transitions = null;
 		private readonly IEnumerable<Context> m_context = null;
 		private readonly IEnumerable<IStatePreProcessor> m_statePreProcessors = null;
 		private readonly IEnumerable<IStatePostProcessor> m_statePostProcessors = null;
@@ -22,12 +19,12 @@ namespace Utilities.States
 		public IState PreviousState { get; private set; }
 
 		public StateMachine(
-			IEnumerable<IStateLogicExecutor> stateLogicExecutor, 
+			IEnumerable<IStateLogicExecutor> stateLogicExecutor,
 			IEnumerable<IStateTransition> transitions,
 			IEnumerable<Context> context,
-            IEnumerable<IStatePreProcessor> statePreProcessor = null, 
+			IEnumerable<IStatePreProcessor> statePreProcessor = null,
 			IEnumerable<IStatePostProcessor> statePostProcessor = null)
-            : this(nameof(StateMachine), stateLogicExecutor, transitions, context, statePreProcessor, statePostProcessor)
+			: this(nameof(StateMachine), stateLogicExecutor, transitions, context, statePreProcessor, statePostProcessor)
 		{
 		}
 
@@ -47,7 +44,7 @@ namespace Utilities.States
 			m_statePostProcessors = statePostProcessor;
 		}
 
-        public void EnterState(IState statToEnter)
+		public void EnterState(IState statToEnter)
 		{
 			if (CurrentState == statToEnter) return;
 
@@ -68,78 +65,13 @@ namespace Utilities.States
 
 			CurrentState = statToEnter;
 
-			FillState(CurrentState, m_context);
+			m_contextHandler.FillState(CurrentState, m_context);
 
 			m_statePreProcessors.Process(CurrentState);
 			CurrentState?.Enter();
 			OnStateChanged?.Invoke(CurrentState);
-			
+
 			SetStateLogic();
-		}
-
-		private bool ValidateType(Context context, MemberInfo member)
-		{
-			var contextType = context.Type;
-			Type memberType = default;
-
-			switch (member)
-			{
-				case FieldInfo fieldInfo:
-					memberType = fieldInfo.FieldType;
-					break;
-				case PropertyInfo propertyInfo:
-					memberType = propertyInfo.PropertyType;
-					break;
-			}
-
-			if (memberType.IsInterface)
-			{
-				var interfaces = contextType.GetInterfaces();
-				return interfaces.Contains(memberType);
-			}
-
-			return contextType == memberType;
-		}
-
-		private void FillState(IState state, IEnumerable<Context> contexts)
-		{
-			var contextDestinations = state.GetContextDestination();
-			foreach (var logic in contextDestinations)
-			{
-				var logicType = logic.GetType();
-				var members = logicType.GetMembers(Binding_Flags);
-
-				foreach (var member in members)
-				{
-					var attribute = member.GetCustomAttribute<ContextField>();
-					if (attribute == null) continue;
-
-					Context context = default;
-					var attributeID = attribute.ID;
-					if (string.IsNullOrEmpty(attributeID))
-					{
-						context = contexts.FirstOrDefault(context => ValidateType(context, member));
-						if (context == null)
-							continue;
-					}
-					else
-					{
-						context = contexts.FirstOrDefault(context => ValidateType(context, member) && context.Id == attributeID);
-						if (context == null)
-							continue;
-					}
-
-					switch(member)
-					{
-						case FieldInfo fieldInfo:
-							fieldInfo.SetValue(logic, context.Object);
-							break;
-						case PropertyInfo propertyInfo:
-							propertyInfo.SetValue(logic, context.Object);
-							break;
-					}
-				}
-			}
 		}
 
 		public void SetStateLogic()
@@ -147,7 +79,7 @@ namespace Utilities.States
 			foreach (var stateLogicExecutor in m_stateLogicExecutor)
 				stateLogicExecutor.SetLogicToExecute(CurrentState);
 		}
-		
+
 		public void RemoveStateLogic()
 		{
 			foreach (var stateLogicExecutor in m_stateLogicExecutor)
