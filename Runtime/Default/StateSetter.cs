d858dfdb44b936abccb11d50e4574f654343f2a2
Łukasz Dąbek <ukidabek@gmail.com>
2024-07-01 20:45:54 +0200

    [Code] StateSetter initialization moved form Avake to SetState method.

--- Runtime/Default/StateSetter.cs
+++ Runtime/Default/StateSetter.cs
@@ -12,29 +12,31 @@ namespace Utilities.States.Default
 #endif
 
 		private IStateSetter m_setter = null;
-		
-		public IState State 
+
+		public IState State
 		{
-			get => m_setter.State; 
-			set => m_setter.State = value; 
+			get => m_setter.State;
+			set => m_setter.State = value;
 		}
 
-		public IStateMachine StateMachine 
-		{ 
-			get => m_setter.StateMachine; 
-			set => m_setter.StateMachine = value; 
+		public IStateMachine StateMachine
+		{
+			get => m_setter.StateMachine;
+			set => m_setter.StateMachine = value;
 		}
 
 		[SerializeField] protected Object _stateMachineObject = null;
 		[SerializeField] protected State m_state = null;
 		[SerializeField] protected bool m_returnToPreviousState = false;
 
-		private void Awake()
+		public void SetState()
 		{
-			Assert.IsTrue(_stateMachineObject is IStateMachine);
-			m_setter = new States.StateSetter(_stateMachineObject as IStateMachine, m_state);
+			if (m_setter == null)
+			{
+				Assert.IsTrue(_stateMachineObject is IStateMachine);
+				m_setter = new States.StateSetter(_stateMachineObject as IStateMachine, m_state);
+			}
+			m_setter.SetState();
 		}
-
-		public void SetState() => m_setter.SetState();
 	}
 }
