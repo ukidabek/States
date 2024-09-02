c7c828cbad206e1a491f82e358594aac88bcf36e
Łukasz Dąbek <ukidabek@gmail.com>
2024-09-02 21:16:20 +0200

    [Code] List form external context is now return thru enumerable property.

--- Runtime/Default/ExternalContext.cs
+++ Runtime/Default/ExternalContext.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using UnityEngine;
 
 namespace Utilities.States.Default
@@ -7,11 +6,6 @@ namespace Utilities.States.Default
 	public class ExternalContext : MonoBehaviour
 	{
 		[SerializeField] private List<Context> m_context = new List<Context>();
-		public ReadOnlyCollection<Context> Context { get; private set; }
-
-		private void Awake()
-		{
-			Context = new ReadOnlyCollection<Context>(m_context);
-		}
+		public IEnumerable<Context> Context => m_context;
 	}
 }
