4b5c060b683f0c1c16eafd3dc550b318ebc921e9
Łukasz Dąbek <ukidabek@gmail.com>
2024-11-19 22:01:16 +0100

    [Code] State executor test removed.

--- Tests Logic/TestStateLogicExecutor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Utilities.States.Test
-{
-	public class TestStateLogicExecutor : StateLogicExecutor<TestUpdateLogic>
-	{
-		public List<TestUpdateLogic> Logic => m_logic;
-	}
-}
